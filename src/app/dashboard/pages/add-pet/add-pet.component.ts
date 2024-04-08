ad671ec1b6f092b4d3215daa68e4d13b028b42de
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2024-04-08 10:09:42 -0700

    componentes faltantes

---
+++ src/app/dashboard/pages/add-pet/add-pet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pet',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-pet.component.html',
+  styles: ``
+})
+export class AddPetComponent {
+
+}
