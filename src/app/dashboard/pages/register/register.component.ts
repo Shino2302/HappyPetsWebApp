f2c5c53529837c0180c182405c50ce92c5a4ffbb
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2024-04-10 13:35:22 -0700

    cambios de ruta

--- src/app/dashboard/pages/register/register.component.ts
+++ src/app/dashboard/pages/register/register.component.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit, input } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { AuthRegister, UsersModel } from '../../../models/users-model';
-import { empty } from 'rxjs';
+import { empty, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -20,7 +20,7 @@ export default class RegisterComponent{
 
   registerForm:FormGroup;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     this.registerForm = new FormGroup({
       name: new FormControl(''),
       email: new FormControl(''),
@@ -46,42 +46,40 @@ export default class RegisterComponent{
   //     return false;
   //   }
   // }
-      registerAuth(): void {
-        let authData: AuthRegister = {
-          email: this.registerForm.get('email')?.value,
-          password: this.registerForm.get('password')?.value,
-          returnSecureToken: true
-        }
-      
-        this.http.post<any>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDSNgDmkvGv6a18hAk0CymT-_EgMORW50A', authData).subscribe(
-          response => {
-            console.log('Todo en orden');
-            console.log(response); // Aquí puedes ver la respuesta completa del servidor
-            this.token = response.idToken;
-            this.uid = response.localId; // Aquí asumimos que el token se encuentra en la propiedad 'idToken' de la respuesta // Aquí puedes ver el token
-          },
-          error => {
-            console.log('Tenemos un problema', error);
-          }
-        );
-        let data:UsersModel = 
-        {
+  registerAuth(): void {
+    let authData: AuthRegister = {
+      email: this.registerForm.get('email')?.value,
+      password: this.registerForm.get('password')?.value,
+      returnSecureToken: true
+    }
+  
+    this.http.post<any>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDSNgDmkvGv6a18hAk0CymT-_EgMORW50A', authData).pipe(
+      switchMap(response => {
+        console.log('Todo en orden');
+        console.log(response); // Aquí puedes ver la respuesta completa del servidor
+        this.token = response.idToken;
+        this.uid = response.localId; // Aquí asumimos que el token se encuentra en la propiedad 'idToken' de la respuesta // Aquí puedes ver el token
+  
+        let data:UsersModel = {
           name: this.registerForm.get('name')?.value,
           email: this.registerForm.get('email')?.value,
           phoneNumber: this.registerForm.get('phoneNumber')?.value,
           password: this.registerForm.get('password')?.value,
           profileImage: this.registerForm.get('profileImage')?.value,
         }
-        this.http.post('https://happydogdb-55b97-default-rtdb.firebaseio.com/Users/'+this.uid.toString()+'.json?auth='+this.token.toString(),data).subscribe(
-          response => {
-          console.log(response);
-          },
-          error =>{
-            console.log('Tenemos un problema', error);
-          }
-        )
+        return this.http.post('https://happydogdb-55b97-default-rtdb.firebaseio.com/Users/'+this.uid.toString()+'.json?auth='+this.token.toString(),data);
+      })
+    ).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['./login']);
+      },
+      error =>{
+        console.log('Tenemos un problema', error);
+        
       }
-      
+    )
+  }
 
   // ngOnInit(): void {
   //   this.debugInputs();
