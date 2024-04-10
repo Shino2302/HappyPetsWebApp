43b02c4afff1190cdb29349d2b6f0f6631dc4082
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2024-04-10 12:15:07 -0700

    Register Ready

--- src/app/dashboard/pages/register/register.component.ts
+++ src/app/dashboard/pages/register/register.component.ts
@@ -14,6 +14,10 @@ import { empty } from 'rxjs';
 // export default class RegisterComponent implements OnInit{
 export default class RegisterComponent{
 
+
+  token:string = "";
+  uid:string = "";
+
   registerForm:FormGroup;
 
   constructor(private http: HttpClient) {
@@ -42,17 +46,6 @@ export default class RegisterComponent{
   //     return false;
   //   }
   // }
-  
-
-   // let data:UsersModel = 
-      // {
-      //   name: this.registerForm.get('name')?.value,
-      //   email: this.registerForm.get('email')?.value,
-      //   phoneNumber: this.registerForm.get('phoneNumber')?.value,
-      //   password: this.registerForm.get('password')?.value,
-      //   profileImage: this.registerForm.get('profileImage')?.value,
-      // }
-
       registerAuth(): void {
         let authData: AuthRegister = {
           email: this.registerForm.get('email')?.value,
@@ -60,17 +53,33 @@ export default class RegisterComponent{
           returnSecureToken: true
         }
       
-        this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDSNgDmkvGv6a18hAk0CymT-_EgMORW50A', authData).subscribe(
+        this.http.post<any>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDSNgDmkvGv6a18hAk0CymT-_EgMORW50A', authData).subscribe(
           response => {
             console.log('Todo en orden');
             console.log(response); // Aquí puedes ver la respuesta completa del servidor
-            let token:any = response; // Aquí asumimos que el token se encuentra en la propiedad 'idToken' de la respuesta
-            console.log(token); // Aquí puedes ver el token
+            this.token = response.idToken;
+            this.uid = response.localId; // Aquí asumimos que el token se encuentra en la propiedad 'idToken' de la respuesta // Aquí puedes ver el token
           },
           error => {
             console.log('Tenemos un problema', error);
           }
         );
+        let data:UsersModel = 
+        {
+          name: this.registerForm.get('name')?.value,
+          email: this.registerForm.get('email')?.value,
+          phoneNumber: this.registerForm.get('phoneNumber')?.value,
+          password: this.registerForm.get('password')?.value,
+          profileImage: this.registerForm.get('profileImage')?.value,
+        }
+        this.http.post('https://happydogdb-55b97-default-rtdb.firebaseio.com/Users/'+this.uid.toString()+'.json?auth='+this.token.toString(),data).subscribe(
+          response => {
+          console.log(response);
+          },
+          error =>{
+            console.log('Tenemos un problema', error);
+          }
+        )
       }
       
 
