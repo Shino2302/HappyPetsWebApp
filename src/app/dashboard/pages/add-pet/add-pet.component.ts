b701b7185027da4c50151ea1ce5730f98232c4ea
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2024-04-11 09:29:21 -0700

    Metodo para formulario de agregar mascota

--- src/app/dashboard/pages/add-pet/add-pet.component.ts
+++ src/app/dashboard/pages/add-pet/add-pet.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PetsModel } from '../../../models/pets-model';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pet',
@@ -12,12 +15,36 @@ export default class AddPetComponent implements OnInit{
 
   uid:any;
   token:any;
+  addPetForm:FormGroup;
 
-  constructor(private route: ActivatedRoute) {  }
+  constructor(private route: ActivatedRoute, private http: HttpClient) 
+  {
+    this.addPetForm = new FormGroup({
+      petAge: new FormControl(''),
+      petImage: new FormControl(''),
+      petName: new FormControl(''),
+      petRace: new FormControl(''),
+      petSize: new FormControl('')
+    })
+  }
+
+  addPet():void{
+
+    let data:PetsModel = {
+      petAge: this.addPetForm.get('petAge')?.value,
+      petImage: this.addPetForm.get('petImage')?.value,
+      petName: this.addPetForm.get('petName')?.value,
+      petRace: this.addPetForm.get('petRace')?.value,
+      petSize: this.addPetForm.get('petSize')?.value
+    };
+    this.http.post('https://happydogdb-55b97-default-rtdb.firebaseio.com/Pets/'+this.uid.toString()+'.json?auth='+this.token.toString(),data);
+  }
 
   ngOnInit(): void {
     this.uid = this.route.snapshot.paramMap.get('uid')?.toString();
     this.token = this.route.snapshot.paramMap.get('token')?.toString();
   }
+
+
 
 }
---
+++ src/app/models/pets-model.ts
@@ -0,0 +1,7 @@
+export class PetsModel {
+    petAge:string = "";
+    petImage:string = "";
+    petName:string = "";
+    petRace:string = "";
+    petSize:string = "";
+}
