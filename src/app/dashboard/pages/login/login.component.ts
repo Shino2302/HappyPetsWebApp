6f16f54633d1b253c0413d36ce4ed5def8712b57
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2024-04-11 06:32:11 -0700

    si

--- src/app/dashboard/pages/login/login.component.ts
+++ src/app/dashboard/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { routes } from '../../../app.routes';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -11,5 +13,25 @@ import { routes } from '../../../app.routes';
 })
 //deifinir a las clases como default para poder utilizarlas como ruta en app.router.ts
 export default class LoginComponent {
-    constructor() {  }
+  
+  loginForm: FormGroup;
+  
+  constructor(private http:HttpClient) {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  loginAction():boolean{
+    try{
+      this.http
+    } 
+    catch{
+      return false;
+    }
+    return true;
+  }
+
+
 }
